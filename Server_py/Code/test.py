3e916ad18af3a2e9566cc7fec317d721ee86237b
Fantasty9413 <969197213@qq.com>
2021-05-12 19:26:33 +0800

    test

--- Server_py/Code/test.py
+++ Server_py/Code/test.py
@@ -1,6 +1,27 @@
-
+import logging
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# 初始化日志对象
+logging.basicConfig(
+ # 日志级别
+level = logging.DEBUG,
+ # 日志格式
+ # 时间、代码所在文件名、代码行号、日志级别名字、日志信息
+format = '%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+ # 打印日志的时间
+datefmt = '%a, %d %b %Y %H:%M:%S',
+# 日志文件存放的目录（目录必须存在）及日志文件名
+filename = '../Log/report.log',
+ # 打开日志文件的方式
+filemode = 'w'
+)
 
 if __name__ == "__main__":
-    f = open('../SourceFile/HttpPackageFile/postconfig.txt')
-    http_package = f.read()
-    print(http_package)
+    # f = open('../SourceFile/HttpPackageFile/postconfig.txt')
+    # http_package = f.read()
+    # print(http_package)
+    str = 'open'
+    logging.debug('str = ' + str)
+    logging.info('str = ' + str)
+    assert str == 'open', 'it is a error.'
+    str = 'close'
+    # assert str == 'open', 'it is a error.'
